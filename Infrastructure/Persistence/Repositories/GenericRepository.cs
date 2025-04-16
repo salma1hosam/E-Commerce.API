16608a5c7bf105f07769e7ffad38dbee1d8ce03b
salma1hosam <salma.hossam2003@gmail.com>
2025-04-16 21:28:52 +0200

    GenericRepository

---
+++ Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,21 @@
+﻿using DomainLayer.Contracts;
+using DomainLayer.Models;
+using Microsoft.EntityFrameworkCore;
+using Persistence.Data;
+
+namespace Persistence.Repositories
+{
+	public class GenericRepository<TEntity, TKey>(StoreDbContext _dbContext) : IGenericRepository<TEntity, TKey> where TEntity : BaseEntity<TKey>
+	{
+
+		public async Task AddAsync(TEntity entity) => await _dbContext.Set<TEntity>().AddAsync(entity);
+
+		public async Task<IEnumerable<TEntity>> GetAllAsync() => await _dbContext.Set<TEntity>().ToListAsync();
+
+		public async Task<TEntity?> GetByIdAsync(TKey id) => await _dbContext.Set<TEntity>().FindAsync(id);
+
+		public void Remove(TEntity entity) => _dbContext.Set<TEntity>().Remove(entity);
+
+		public void Update(TEntity entity) => _dbContext.Set<TEntity>().Update(entity);
+	}
+}
