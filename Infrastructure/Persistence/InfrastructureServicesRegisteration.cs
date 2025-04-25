7b84a35ec5df32d399899c382fdcc8c4a34efa0b
salma1hosam <salma.hossam2003@gmail.com>
2025-04-26 01:00:42 +0300

    Basket Module - Repository

--- Infrastructure/Persistence/InfrastructureServicesRegisteration.cs
+++ Infrastructure/Persistence/InfrastructureServicesRegisteration.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
 
 namespace Persistence
 {
@@ -7,6 +8,7 @@ namespace Persistence
 		//Extension Method To Add all the Persistence Layer (Infrastructur) Services to the Services Container through it
 		public static IServiceCollection AddInfrastructureServices(this IServiceCollection Services, IConfiguration Configuration)
 		{
+			//Adding or Registering the DbContext Service
 			Services.AddDbContext<StoreDbContext>(options =>
 			{
 				options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
@@ -14,6 +16,12 @@ namespace Persistence
 
 			Services.AddScoped<IDataSeeding, DataSeeding>();
 			Services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+			//Adding or Registering the Redis Connection Service
+			Services.AddSingleton<IConnectionMultiplexer>((_) =>
+			{
+				return ConnectionMultiplexer.Connect(Configuration.GetConnectionString("RedisConnectionString"));
+			});
 			return Services;
 		}
 	}
