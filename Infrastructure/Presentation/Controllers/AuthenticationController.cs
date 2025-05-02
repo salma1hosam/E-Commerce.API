db6fd5d8abb5c2de1b3d5ed5c1dcd15de1023358
salma1hosam <salma.hossam2003@gmail.com>
2025-05-02 19:44:19 +0300

    Authentication Controller [Address , Email , and Get User]

--- Infrastructure/Presentation/Controllers/AuthenticationController.cs
+++ Infrastructure/Presentation/Controllers/AuthenticationController.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ServiceAbstraction;
 using Shared.DataTransferObjects.IdentityModule;
+using System.Security.Claims;
 
 namespace Presentation.Controllers
 {
@@ -18,6 +20,40 @@ namespace Presentation.Controllers
 		{
 			var user = await _serviceManager.AuthenticationService.RegisterAsync(registerDto);
 			return Ok(user);
+		}
+
+		[HttpGet("CheckEmail")]
+		public async Task<ActionResult<bool>> CheckEmail(string email)
+		{
+			var result = await _serviceManager.AuthenticationService.CheckEmailAsync(email);
+			return Ok(result);
+		}
+
+		[Authorize]
+		[HttpGet("CurrentUser")]
+		public async Task<ActionResult<UserDto>> GetCurrentUser()
+		{
+			var email = User.FindFirstValue(ClaimTypes.Email);
+			var appUser = await _serviceManager.AuthenticationService.GetCurrentUserAsync(email!);
+			return Ok(appUser);
+		}
+
+		[Authorize]
+		[HttpGet("Address")]
+		public async Task<ActionResult<AddressDto>> GetCurrentUserAddress()
+		{
+			var email = User.FindFirstValue(ClaimTypes.Email);
+			var address = await _serviceManager.AuthenticationService.GetCurrentUserAddressAsync(email!);
+			return Ok(address);
+		}
+
+		[Authorize]
+		[HttpPut("Address")]
+		public async Task<ActionResult<AddressDto>> UpdateCurrentUserAddress(AddressDto addressDto)
+		{
+			var email = User.FindFirstValue(ClaimTypes.Email);
+			var updatedAddress = await _serviceManager.AuthenticationService.UpdateCurrentUserAddressAsync(email, addressDto);
+			return Ok(updatedAddress);
 		}
 	}
 }
