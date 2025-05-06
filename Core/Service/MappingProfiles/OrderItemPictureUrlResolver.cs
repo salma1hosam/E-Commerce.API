848326e80f7ae662230f6e042dc1a086dc2939c1
salma1hosam <salma.hossam2003@gmail.com>
2025-05-06 19:08:01 +0300

    Order Profile [Mapping]

---
+++ Core/Service/MappingProfiles/OrderItemPictureUrlResolver.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using DomainLayer.Models.OrderModule;
+using Microsoft.Extensions.Configuration;
+using Shared.DataTransferObjects.OrderModule;
+
+namespace Service.MappingProfiles
+{
+	internal class OrderItemPictureUrlResolver(IConfiguration _configuration) : IValueResolver<OrderItem, OrderItemDto, string>
+	{
+		public string Resolve(OrderItem source, OrderItemDto destination, string destMember, ResolutionContext context)
+		{
+			if (string.IsNullOrEmpty(source.Product.PictureUrl))
+				return string.Empty;
+			else
+			{
+				var url = $"{_configuration.GetSection("Urls")["BaseUrl"]}{source.Product.PictureUrl}";
+				return url;
+			}
+		}
+	}
+}
--- Core/Service/MappingProfiles/OrderProfile.cs
+++ Core/Service/MappingProfiles/OrderProfile.cs
@@ -9,7 +9,12 @@ namespace Service.MappingProfiles
 	{
 		public OrderProfile()
 		{
-			CreateMap<AddressDto, OrderAddress>();
+			CreateMap<AddressDto, OrderAddress>().ReverseMap();
+			CreateMap<Order, OrderToReturnDto>()
+				.ForMember(dist => dist.DeliveryMethod, options => options.MapFrom(src => src.DeliveryMethod.ShortName));
+			CreateMap<OrderItem, OrderItemDto>()
+				.ForMember(dist => dist.ProductName, options => options.MapFrom(src => src.Product.ProductName))
+				.ForMember(dist => dist.PictureUrl, options => options.MapFrom<OrderItemPictureUrlResolver>());
 		}
 	}
 }
